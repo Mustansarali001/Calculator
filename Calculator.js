beba30e23bcc83949fb4fcea4e6c7b83251d129f
Mustansar Ali <138989615+Mustansarali001@users.noreply.github.com>
2023-07-20 08:48:45 +0500

    Add files via upload

--- Calculator.js
+++ Calculator.js
@@ -8,22 +8,30 @@ arr.forEach(button => {
     button.addEventListener('click', (e) => {
         if (e.target.innerHTML == '=') {
             string = eval(string);
-            input.value = string;
+
+            if(string == 'Infinity'){
+                input.value= 'Invalid Division';
+            }
+            else{
+                input.value = string;
+            }
         }
 
         else if (e.target.innerHTML == 'AC') {
             string = "";
             input.value = string;
         }
-        else if (e.target.innerHTML == 'DEL') {
-                string = string.substring(0, string.length - 1);
-                input.value = string;
-            }
-            else {
-                string += e.target.innerHTML;
-                input.value = string;
-            }
 
-        })
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+
+    })
 
 })
