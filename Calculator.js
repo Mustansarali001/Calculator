93bf90d1bfbfb662b6f873d64c2a3c291f4c3b77
Mustansar Ali <138989615+Mustansarali001@users.noreply.github.com>
2023-07-22 21:47:26 +0500

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,9 +1,9 @@
 let input = document.getElementById('inputbox');
-let buttons = document.querySelectorAll
-    ('button');
+let buttons = document.querySelectorAll('button');
 
 let string = "";
 let arr = Array.from(buttons);
+
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
         if (e.target.innerHTML == '=') {
